964899d34c54c33d6b0c220dc865e054faba74e6
zhudm <601443405@qq.com>
2017-04-14 22:20:54 +0800

    取消动作加上

--- src/js/controller/order_success.js
+++ src/js/controller/order_success.js
@@ -5,13 +5,29 @@ angular.module("ctApp")
 		"$apis",
 		"$timeout",
 		function($scope, $state, $apis, $timeout) {
+			var types = ["保养","喷漆","美容","陪同协助"];
 			$scope.shop = {
 				name:"深圳宝源宝马4S店",
 				address:"深圳市福田区梅林街道北环大道7108号",
-				type:1,//0-保养，1-喷漆，2-美容，3-陪同协助
+				type:types[1],//0-保养，1-喷漆，2-美容，3-陪同协助
 				date:"2017-04-17",
 				time:"09:00-12:00",
-				icon:"/images/none.jpg"
+				icon:"/images/none.jpg",
+				canceled:false
 			};
+
+			$scope.canceling = false;
+			$scope.cancelOrder = function(){
+				if($scope.canceling){
+					return;
+				}
+				$scope.canceling = true;
+				var load = weui.loading("正在取消...");
+				$timeout(function(){
+					$scope.canceling = false;
+					$scope.shop.canceled = true;
+					load.hide();
+				},2000);
+			}
 		}
 	]);
