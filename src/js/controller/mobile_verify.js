112c7922e83077ad4269f973ff585f554270392a
zhudm <zhudm@chetong.net>
2017-04-18 14:33:27 +0800

    优化验证手机号页面

--- src/js/controller/mobile_verify.js
+++ src/js/controller/mobile_verify.js
@@ -26,51 +26,49 @@ angular.module("ctApp")
 					weui.topTips('请输入手机号');
 					return;
 				}
-				if (reg_mobile.test(mobile)) {
-					if ($scope.oldItem.sending || $scope.oldItem.second > 0) { //正在发送或正在倒计时
-						return;
-					}
-					$scope.oldItem.sending = true;
-					mobilecode.send(mobile, function() {
-						$scope.oldItem.second = 60;
-						$scope.oldItem.timer = $interval(function() {
-							if ($scope.oldItem.second > 1) {
-								$scope.oldItem.second--;
-							} else {
-								$interval.cancel($scope.oldItem.timer);
-								$scope.oldItem.timer = null;
-								$scope.oldItem.second = 0;
-							}
-						}, 1000);
-						$scope.oldItem.sending = false;
-					}, function() {
-						$scope.oldItem.second = 60;
-						$scope.oldItem.timer = $interval(function() {
-							if ($scope.oldItem.second > 1) {
-								$scope.oldItem.second--;
-							} else {
-								$interval.cancel($scope.oldItem.timer);
-								$scope.oldItem.timer = null;
-								$scope.oldItem.second = 0;
-							}
-						}, 1000);
-						$scope.oldItem.sending = false;
-					});
-				} else {
+				if (!reg_mobile.test(mobile)) {
 					weui.topTips('手机号格式不正确');
+					return;
 				}
+				if ($scope.oldItem.sending || $scope.oldItem.second > 0) { //正在发送或正在倒计时
+					return;
+				}
+				$scope.oldItem.sending = true;
+				mobilecode.send(mobile, function() {
+					$scope.oldItem.second = 60;
+					$scope.oldItem.timer = $interval(function() {
+						if ($scope.oldItem.second > 1) {
+							$scope.oldItem.second--;
+						} else {
+							$interval.cancel($scope.oldItem.timer);
+							$scope.oldItem.timer = null;
+							$scope.oldItem.second = 0;
+						}
+					}, 1000);
+					$scope.oldItem.sending = false;
+				}, function() {
+					$scope.oldItem.second = 60;
+					$scope.oldItem.timer = $interval(function() {
+						if ($scope.oldItem.second > 1) {
+							$scope.oldItem.second--;
+						} else {
+							$interval.cancel($scope.oldItem.timer);
+							$scope.oldItem.timer = null;
+							$scope.oldItem.second = 0;
+						}
+					}, 1000);
+					$scope.oldItem.sending = false;
+				});
 			};
 
-			$timeout(function() {
-				weui.form.checkIfBlur('#mobile-old-form', {
-					regexp: {
-						MOBILE: reg_mobile
-					}
-				});
-			}, 1500);
+			weui.form.checkIfBlur('#mobile-old-form', {
+				regexp: {
+					MOBILE: reg_mobile
+				}
+			});
 
 			$scope.verify = function() {
-				weui.form.validate('#mobile-old-form', function(error) {
+				weui.form.validate('#mobile-verify-page-form', function(error) {
 					if (!error) { //hide-form
 						var loading = weui.loading('提交中...');
 						$timeout(function() {
